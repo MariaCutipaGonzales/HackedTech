ce590ef00d0a1778a23492221df32676ce5e89e7
Maria <mariacutipa.gonzales@gmail.com>
2017-04-29 21:37:21 -0500

    SliderQuestions listo

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+/*SLIDE SHOW*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+     }
+     x[slideIndex-1].style.display = "block";  
+}
+
+document.getElementById("opc-1").addEventListener("click",function(e){
+	activate("opc1",e);
+});
+
+document.getElementById("opc-2").addEventListener("click",function(e){
+	activate("opc2",e);
+});
+
+document.getElementById("opc-3").addEventListener("click",function(e){
+	activate("opc3",e);
+});
+
+document.getElementById("opc-4").addEventListener("click",function(e){
+	activate("opc4",e);
+});
+
+ function activate(clase,event){
+ 	var opcion =document.getElementsByClassName(""+clase+"");
+	for (var i = 0; i < opcion.length; i++) {
+		opcion[i].classList.value="cnt-option "+clase;
+	}
+ 		if (event.target.tagName == "IMG") {
+		event.target.parentNode.classList.add("activated");
+	}
+ }
